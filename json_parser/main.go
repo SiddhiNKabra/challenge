58460cbf6724daf3a5c664afbe227b777de8e26e
SiddhiNKabra <nksiddhi20@gmail.com>
2024-02-19 22:51:13 +0530

    parser

--- json_parser/main.go
+++ json_parser/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+func jsonparser(name string) (map[string]interface{}, error) {
+	file, err := os.Open(name)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+	c, err := io.ReadAll(file)
+	if err != nil {
+		return nil, err
+	}
+
+	var dataparser map[string]interface{}
+
+}
 
 func main() {
-	fmt.Println("Hello World")
+	fmt.Println("JSON Parser")
 }
