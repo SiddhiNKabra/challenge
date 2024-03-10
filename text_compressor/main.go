b15611a553e151da221672f00a38808096e59aeb
SiddhiNKabra <nksiddhi20@gmail.com>
2024-03-10 16:51:38 +0530

    text compressor using huffman algo

--- text_compressor/main.go
+++ text_compressor/main.go
@@ -18,6 +18,20 @@ func (h Huffmanheap) Len() int           { return len(h) }
 func (h Huffmanheap) Less(i, j int) bool { return h[i].freq < h[j].freq }
 func (h Huffmanheap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
 
+func (h *Huffmanheap) Push(x interface{}) {
+	*h = append(*h, x.(*Node))
+}
+
+func (h *Huffmanheap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	node := old[n-1]
+	*h = old[0:n-1]
+	return node
+}
+
+
+
 func main() {
 	fmt.Println("Hello")
 }
