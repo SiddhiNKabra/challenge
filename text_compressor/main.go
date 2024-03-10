c79b2828a9fd91cb2f586162c5b9ed3801080c8e
SiddhiNKabra <nksiddhi20@gmail.com>
2024-03-10 16:48:18 +0530

    text compressor using huffman algo

--- text_compressor/main.go
+++ text_compressor/main.go
@@ -1,9 +1,23 @@
 package main
 
-import(
+import (
+	"container/heap"
 	"fmt"
 )
 
-func main(){
+type Node struct {
+	char  byte
+	freq  int
+	left  *Node
+	right *Node
+}
+
+type Huffmanheap []*Node
+
+func (h Huffmanheap) Len() int           { return len(h) }
+func (h Huffmanheap) Less(i, j int) bool { return h[i].freq < h[j].freq }
+func (h Huffmanheap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+
+func main() {
 	fmt.Println("Hello")
 }
